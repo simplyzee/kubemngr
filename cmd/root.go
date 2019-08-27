826d4531e9c2c381ac1d01a37b6915f56596994c
Zee <zeieshanahmed@icloud.com>
2019-08-28 00:34:46 +0100

    Adding updates for root cli command

--- cmd/root.go
+++ cmd/root.go
@@ -58,7 +58,7 @@ func init() {
   // Cobra supports persistent flags, which, if defined here,
   // will be global for your application.
 
-  rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kubemngr.yaml)")
+  //rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kubemngr.yaml)")
 
 
   // Cobra also supports local flags, which will only run
