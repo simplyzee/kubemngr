3724ef782e92fd6cba43d34f04f8790d10a19522
Zee <zeieshanahmed@icloud.com>
2019-09-12 18:07:09 +0100

    Adding remove command which checks for version and delete if it exists

--- cmd/remove.go
+++ cmd/remove.go
@@ -17,6 +17,8 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -32,7 +34,10 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("remove called")
+		err := RemoveKubectlVersion(args[0])
+		if err != nil {
+			log.Fatal(err)
+		}
 	},
 }
 
@@ -48,4 +53,26 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// removeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// RemoveKubectlVersion - removes specific kubectl version from machine
+func RemoveKubectlVersion(version string) error {
+	// Get user home directory path
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	kubectlVersion := homeDir + "/.kubemngr/kubectl-" + version
+
+	// Check if version exists to remove it
+	_, err = os.Stat(kubectlVersion)
+	if err == nil {
+		fmt.Printf("Removing kubectl %s", version)
+		os.Remove(kubectlVersion)
+		return nil
+	} else {
+		fmt.Printf("kubectl version %s doesn't exist", version)
+		return nil
+	}
 }
